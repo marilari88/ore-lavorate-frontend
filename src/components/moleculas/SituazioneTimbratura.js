368aa50fc183fda8eab5879325098ec243e30ea9
Marco Ilari <marilari88@gmail.com>
2020-09-06 13:10:17 +0200

    Iniziata l'implementazione dello swipe per la gestione della timbratura

--- src/components/moleculas/SituazioneTimbratura.js
+++ src/components/moleculas/SituazioneTimbratura.js
@@ -1,10 +1,16 @@
 import React from "react";
+import SwipeableButton from "../atoms/SwipeableButton";
 
 function SituazioneTimbratura() {
   return (
     <div className="situazioneTimbratura">
       <div className="etichettaSituazione">Stai lavorando da:</div>
       <div className="tempoLavoroInCorso">7 ore, 15 minuti e 16 secondi</div>
+      <SwipeableButton
+        color="#6ab04c"
+        text="INIZIA A LAVORARE"
+        onSuccess={() => console.log("ciao")}
+      />
     </div>
   );
 }
