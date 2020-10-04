8b50f87f607145a1bb2170aaceb0c806a8967c15
Marco Ilari <marilari88@gmail.com>
2020-10-05 00:19:59 +0200

    Implementato logout

--- src/components/atoms/InfoLogin.js
+++ src/components/atoms/InfoLogin.js
@@ -1,11 +1,52 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, Menu, MenuItem } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-function InfoLogin({ userData }) {
+function InfoLogin({ userData, setUserData }) {
+  const history = useHistory();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const logOut = () => {
+    localStorage.removeItem("auth-token");
+    setUserData({});
+    history.push("/login");
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(document.querySelector(".nomeOperatore"));
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="infoLogin">
-      <div className="nomeOperatore">{userData && userData.name}</div>
+      <div
+        aria-controls="user-menu"
+        aria-haspopup="true"
+        className="nomeOperatore"
+        onClick={handleClick}
+      >
+        {userData && userData.name}
+      </div>
       <Avatar alt="{{userData && user.name}}"></Avatar>
+      <Menu
+        id="user-menu"
+        anchorEl={document.getElementsByClassName("nomeOperatore")}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>Il mio Profilo</MenuItem>
+        <MenuItem
+          onClick={() => {
+            logOut();
+          }}
+        >
+          Logout
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -4,13 +4,17 @@ import InfoContratto from "../atoms/InfoContratto";
 import { useAuth } from "../../context/UserContext";
 
 function Header() {
-  const { userData } = useAuth();
+  const { userData, setUserData } = useAuth();
 
   return (
     <header>
       <nav className="topBar">
         <InfoContratto />
-        {userData ? <InfoLogin userData={userData} /> : ""}
+        {userData ? (
+          <InfoLogin userData={userData} setUserData={setUserData} />
+        ) : (
+          ""
+        )}
       </nav>
     </header>
   );
