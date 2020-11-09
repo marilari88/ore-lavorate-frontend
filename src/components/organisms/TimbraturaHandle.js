b042913ec3cc21811834d21792f2905b5e37afe5
marilari88 <marilari88@gmail.com>
2020-11-09 01:05:06 +0100

    Eseguito styling della maschera di visualizzazione della singola timbratura, Aggiunto il pulsante per la cancellazione della timbratura, Indicatore per turni notturni

--- src/components/organisms/TimbraturaHandle.js
+++ src/components/organisms/TimbraturaHandle.js
@@ -1,16 +1,53 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { stringaGiorno, stringaOrario } from "../../utils/datetime";
+import { stringaTempoBreve } from "../../utils/differenzaorario";
+import CallMadeIcon from "@material-ui/icons/CallMade";
+import PanToolIcon from "@material-ui/icons/PanTool";
 
 export default function TimbraturaHandle({ timbraturaSelezionata }) {
-  const history = useHistory();
+  const [giornoSeguente, setGiornoSeguente] = useState(true);
+
+  useEffect(() => {
+    const ingressoDate = new Date(timbraturaSelezionata.ingresso);
+    const uscitaDate = new Date(timbraturaSelezionata.uscita);
+    const giornoUscita = uscitaDate.getDay();
+    const giornoIngresso = ingressoDate.getDay();
+    setGiornoSeguente(giornoUscita > giornoIngresso);
+    console.log(giornoUscita, giornoIngresso);
+  }, [timbraturaSelezionata]);
+
   return (
     <div className="timbraturaHandle">
-      <div className="giornoTimbratura">${timbraturaSelezionata.ingresso}</div>
-      <div className="giornoTimbratura">${timbraturaSelezionata.ingresso}</div>
-      <div className="giornoTimbratura">${timbraturaSelezionata.uscita}</div>
-      <button className="pulsante" onClick={() => history.push("/")}>
-        Indietro
-      </button>
+      <h2 className="giornoTimbratura">
+        {stringaGiorno(timbraturaSelezionata.ingresso)}
+      </h2>
+      <div className="rigaHandleTimbratura">
+        <CallMadeIcon style={{ color: "#a2e88b" }} /> Sei entrato alle{" "}
+        <div className="orarioIngresso">
+          {stringaOrario(timbraturaSelezionata.ingresso)}
+        </div>
+      </div>
+      <div className="rigaHandleTimbratura">
+        <CallMadeIcon
+          style={{ color: "#f26d6d", transform: "rotate(90deg)" }}
+        />
+        Sei uscito alle{" "}
+        <div className={`orarioUscita ${giornoSeguente && "giornoSeguente"}`}>
+          {stringaOrario(timbraturaSelezionata.uscita)}
+        </div>
+      </div>
+      <div className="tempoLavorato">
+        Hai lavorato per {stringaTempoBreve(timbraturaSelezionata.differenza)}
+      </div>
+      <div className="timbraturaManuale">
+        <PanToolIcon className="iconaMano" />
+        <div className="testoTimbraturaManuale">
+          La timbratura è stata modificata manualmente
+        </div>
+      </div>
+      <div className="rigaPulsantiTimbratura">
+        <button className="pulsante pulsanteCancella">Elimina</button>
+      </div>
     </div>
   );
 }
