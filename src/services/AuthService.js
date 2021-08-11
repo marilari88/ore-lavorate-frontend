f98081df9bd0d667cff05bb21073a18e7baa25f1
Marco Ilari <marilari88@gmail.com>
2021-08-12 00:55:07 +0200

    Fix indentation

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,32 +2,32 @@ import axios from "axios";
 import AuthHeader from "./AuthHeader";
 
 class AuthService {
-    constructor() {
-        this.URL = process.env.REACT_APP_PUBLIC_API_URL;
-    }
+  constructor() {
+    this.URL = process.env.REACT_APP_PUBLIC_API_URL;
+  }
 
-    async checkToken() {
-        const token = localStorage.getItem("auth-token");
-        if (token) {
-            return await axios.get(this.URL + "/user/checktoken", AuthHeader());
-        } else {
-            return;
-        }
+  async checkToken() {
+    const token = localStorage.getItem("auth-token");
+    if (token) {
+      return await axios.get(this.URL + "/user/checktoken", AuthHeader());
+    } else {
+      return;
     }
+  }
 
-    async registerUser(user) {
-        return await axios.post(this.URL + "/user/register", user);
-    }
+  async registerUser(user) {
+    return await axios.post(this.URL + "/user/register", user);
+  }
 
-    async loginUser(user) {
-        return await axios.post(this.URL + "/user/login", user);
-    }
+  async loginUser(user) {
+    return await axios.post(this.URL + "/user/login", user);
+  }
 
-    async loginGoogleUser(tokenId) {
-        const data = {
-            tokenid: tokenId,
-        };
-        return await axios.post(this.URL + "/user/googlelogin", data);
-    }
+  async loginGoogleUser(tokenId) {
+    const data = {
+      tokenid: tokenId,
+    };
+    return await axios.post(this.URL + "/user/googlelogin", data);
+  }
 }
 export default new AuthService();
