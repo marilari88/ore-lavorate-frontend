b1ea31d9bfff833b456fb5447729c75af0de6a79
Marco Ilari <marilari88@gmail.com>
2021-08-17 00:16:03 +0200

    Delete components not used

--- src/components/atoms/TextInput.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TextInput({ value }) {
-  return (
-    <>
-      <input type="text" className="orarioUscita" value={value} />
-    </>
-  );
-}
