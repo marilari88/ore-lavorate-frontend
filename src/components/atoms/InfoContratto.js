52744ebe89b0d539fb9e1aabc70dcecd4aa2ab80
Marco Ilari <marilari88@gmail.com>
2021-08-16 23:18:28 +0200

    Add menu gestione contratto

--- src/components/atoms/InfoContratto.js
+++ src/components/atoms/InfoContratto.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { Menu, MenuItem } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 function InfoContratto() {
+  const history = useHistory();
+
+  const [anchorEl, setAnchorEl] = useState("");
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="infoContratto">
-      Fatebenefratelli
-      <ArrowDropDownIcon />
-    </div>
+    <>
+      <div
+        aria-controls="contratto-menu"
+        className="infoContratto"
+        aria-haspopup="true"
+        aria-owns={anchorEl ? "contratto-menu" : undefined}
+        onClick={handleClick}
+      >
+        Fatebenefratelli
+        <ArrowDropDownIcon />
+      </div>
+      <Menu
+        id="contratto-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <MenuItem
+          onClick={() => {
+            history.push("/");
+            handleClose();
+          }}
+        >
+          Situazione Corrente
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            history.push("/elencotimbrature");
+            handleClose();
+          }}
+        >
+          Timbrature
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            history.push("profile");
+            handleClose();
+          }}
+        >
+          Cambia Contratto
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
 
