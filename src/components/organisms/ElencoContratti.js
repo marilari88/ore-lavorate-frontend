a25f556745b117dd8b46ec95b6b52884b525e6e7
Marco Ilari <marilari88@gmail.com>
2021-08-28 17:59:38 +0200

    ElencoContratti added loader

--- src/components/organisms/ElencoContratti.js
+++ src/components/organisms/ElencoContratti.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import ContrattoButton from "../moleculas/ContrattoButton";
 import ContrattoDataService from "../../services/ContrattoService";
+import Loader from "react-loader-spinner";
 
 function ElencoContratti({ setContrattoSelezionato, setContrattoEdit }) {
   const [elencoContratti, setElencoContratti] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     caricaElencoContratti();
@@ -12,37 +14,50 @@ function ElencoContratti({ setContrattoSelezionato, setContrattoEdit }) {
   const caricaElencoContratti = async () => {
     const contrattiCaricati = await ContrattoDataService.getAll();
     setElencoContratti(contrattiCaricati.data);
+    setIsLoading(false);
   };
 
   return (
     <div>
       <h1>Elenco Contratti</h1>
-      <div className="riassuntoContratti">
-        Hai un totale di {elencoContratti.length} 📗
-      </div>
-      <div className="elencoContratti">
-        <ContrattoButton
-          id={null}
-          nomeAzienda={null}
-          nomeContratto={null}
-          dataInizio={null}
-          dataFine={null}
-          setContrattoSelezionato={setContrattoSelezionato}
-          setContrattoEdit={setContrattoEdit}
+      {isLoading ? (
+        <Loader
+          type="ThreeDots"
+          color="#89c5cc"
+          height={40}
+          width={40}
+          className="loader"
         />
-        {elencoContratti &&
-          Array.from(elencoContratti).map((contratto) => (
+      ) : (
+        <>
+          <div className="riassuntoContratti">
+            Hai un totale di {elencoContratti.length} 📗
+          </div>
+          <div className="elencoContratti">
             <ContrattoButton
-              key={contratto._id}
-              id={contratto._id}
-              nomeAzienda={contratto.nomeAzienda}
-              nomeContratto={contratto.nomeContratto}
-              dataInizio={contratto.inizioContratto}
-              dataFine={contratto.fineContratto}
+              id={null}
+              nomeAzienda={null}
+              nomeContratto={null}
+              dataInizio={null}
+              dataFine={null}
               setContrattoSelezionato={setContrattoSelezionato}
+              setContrattoEdit={setContrattoEdit}
             />
-          ))}
-      </div>
+            {elencoContratti &&
+              Array.from(elencoContratti).map((contratto) => (
+                <ContrattoButton
+                  key={contratto._id}
+                  id={contratto._id}
+                  nomeAzienda={contratto.nomeAzienda}
+                  nomeContratto={contratto.nomeContratto}
+                  dataInizio={contratto.inizioContratto}
+                  dataFine={contratto.fineContratto}
+                  setContrattoSelezionato={setContrattoSelezionato}
+                />
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
